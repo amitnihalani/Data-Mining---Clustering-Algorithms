65b46acb912837210edfa7a2a377a88327b68aee
amitnihalani <ami.nihalani@gmail.com>
2015-11-07 00:08:10 -0500

    Added class for cluster

---
+++ src/com/company/beans/Cluster.java
@@ -0,0 +1,49 @@
+package com.company.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Amit on 11/6/2015.
+ */
+public class Cluster {
+
+    private List<Gene> genes;
+    private int clusterId;
+    private List<Double> centroid;
+
+    public Cluster(List<Double> head, int cId) {
+        centroid = head;
+        genes = new ArrayList<>();
+        clusterId = cId;
+    }
+
+    public List<Gene> getGenes() {
+        return genes;
+    }
+
+    public void addGenes(List<Gene> genes) {
+        this.genes.addAll(genes);
+    }
+
+    public void addGene(Gene gene) {
+        this.genes.add(gene);
+    }
+
+    public int getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(int clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public List<Double> getCentroid() {
+        return centroid;
+    }
+
+    public void setCentroid(List<Double> centroid) {
+        this.centroid = centroid;
+    }
+
+}
--- src/com/company/beans/Gene.java
+++ src/com/company/beans/Gene.java
@@ -8,10 +8,11 @@ import java.util.List;
  * Created by Amit on 10/31/2015.
  */
 public class Gene {
-    int geneId;
-    int clusterId;
-    List<Double> expressionValues = new ArrayList<Double>();
+    private int geneId;
+    private int clusterId;
+    private List<Double> expressionValues = new ArrayList<Double>();
 
+    // Constructor for Gene.
     public Gene(int id, int cId, String[] expressionValueList) {
         geneId = id;
         clusterId = cId;
@@ -19,4 +20,38 @@ public class Gene {
             expressionValues.add(Double.parseDouble(expressionValue));
         }
     }
+
+    public Gene(int id, String[] expressionValueList) {
+        geneId = id;
+        clusterId = -99;
+        for(String expressionValue:expressionValueList) {
+            expressionValues.add(Double.parseDouble(expressionValue));
+        }
+    }
+
+    // Getters and setters
+    public int getGeneId() {
+        return geneId;
+    }
+
+    public void setGeneId(int geneId) {
+        this.geneId = geneId;
+    }
+
+    public int getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(int clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public List<Double> getExpressionValues() {
+        return expressionValues;
+    }
+
+    public void setExpressionValues(List<Double> expressionValues) {
+        this.expressionValues = expressionValues;
+    }
+
 }
